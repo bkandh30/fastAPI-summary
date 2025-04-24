9de53e0f9cf9252de9588ef1a50808858faa53bd
bkandh30 <kandhari.bhavya20@gmail.com>
2025-04-23 21:55:46 -0700

    code restructure and database init

--- project/app/main.py
+++ project/app/main.py
@@ -1,26 +1,18 @@
-import os
+import logging
 
-from fastapi import FastAPI, Depends
-from tortoise.contrib.fastapi import register_tortoise
-
-from app.config import get_settings, Settings
+from fastapi import FastAPI
 
 from app.api import ping
+from app.db import init_db
+
+log = logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models":["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
 
     return application
 
-
 app = create_application()
+
+init_db(app)

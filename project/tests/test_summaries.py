1f731f6db7890d3c6d1b8471b283c2bd5268d9aa
bkandh30 <kandhari.bhavya20@gmail.com>
2025-04-25 15:03:37 -0700

    added get route validation

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -38,9 +38,24 @@ def test_read_summary(test_app_with_db):
     assert response_dict["created_at"]
 
 def test_read_summary_incorrect_id(test_app_with_db):
-    response = test_app_with_db.get("/summaries/999")
+    response = test_app_with_db.get("/summaries/999/")
     assert response.status_code == 404
     assert response.json()["detail"] == "Summary not found"
+
+    response = test_app_with_db.get("/summaries/0/")
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": [
+            {
+                "ctx": {"gt": 0},
+                "input": "0",
+                "loc": ["path", "id"],
+                "msg": "Input should be greater than 0",
+                "type": "greater_than",
+            }
+        ]
+    }
+
 
 def test_read_all_summaries(test_app_with_db):
     response = test_app_with_db.post("/summaries", data=json.dumps({"url": "https://foo.bar"}))

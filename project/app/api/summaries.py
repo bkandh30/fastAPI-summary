8818768fab26b81583cf9ca65aa7eaccd0bc7784
bkandh30 <kandhari.bhavya20@gmail.com>
2025-04-25 15:26:16 -0700

    added builder stage

--- project/app/api/summaries.py
+++ project/app/api/summaries.py
@@ -25,7 +25,7 @@ async def create_summary(payload: SummaryPayloadSchema) -> SummaryResponseSchema
     return response_object
 
 
-@router.get("/{id}", response_model=SummarySchema)
+@router.get("/{id}/", response_model=SummarySchema)
 async def read_summary(id: int = Path(..., gt=0)) -> SummarySchema:
     summary = await crud.get(id)
     if not summary:
@@ -39,11 +39,12 @@ async def read_all_summaries() -> List[SummarySchema]:
     return await crud.get_all()
 
 
-@router.delete("/{id}", response_model=SummaryResponseSchema)
-async def delete_summary(id: int) -> SummaryResponseSchema:
+@router.delete("/{id}/", response_model=SummaryResponseSchema)
+async def delete_summary(id: int = Path(..., gt=0)) -> SummaryResponseSchema:
     summary = await crud.get(id)
     if not summary:
         raise HTTPException(status_code=404, detail="Summary not found")
+
     await crud.delete(id)
 
     return summary
